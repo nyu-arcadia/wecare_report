9591aec1648d341858b7c846c427160a03771d83
Zezhen Wu <wuzezhen33@gmail.com>
2024-04-18 16:57:37 -0400

    fix errors in report

--- R/load_data.R
+++ R/load_data.R
@@ -12,7 +12,54 @@ dat <- read_csv(file.path(box_path, "cleaned/dat_merged.csv")) |>
     screen_age_group == 1 ~ "18+",
     TRUE ~ NA_character_  # Handles cases that do not fit either category
   )) |> 
+  rowwise() |> 
+  mutate(screen_race_eligible = if_any(c(screen_race_1, screen_race_2, screen_race_3, screen_race_4), 
+                                     ~ .x == 1)) |> 
+  mutate(completed_survey = if_else(
+    any(!is.na(c_across(starts_with(c("giso_","cssrs_", "st_", "ets_",
+                                      "acfs_", "barrier_", "atphs_", "soc_",
+                                      "tam_", "pfcs_", "pvp_", "cde_",
+                                      "hopeless_", "uppss_", "yrbsa_", "dus_",
+                                      "se_", "pet_", "pil_", "cdef_",
+                                      "ibelong_", "sc_", "joy_")) &
+                        ends_with("_b")
+                        ))),
+    1, 0
+  )) |> 
+  ungroup() |>
   mutate(site_id = if_else(site_id == "H", "Harlem", "Kings County")) |> 
-  mutate(treatment = if_else(treatment == 1, "Treatment", "Control"))
+  mutate(treatment = if_else(treatment == 1, "Treatment", "Control")) |> 
+  mutate(
+    meet_minimal_risk = if_else(
+      eligible_visited_ed == 1 | 
+      eligible_hospitalized == 1 | 
+      eligible_taken_med == 1 | 
+      eligible_received_therapy == 1 | 
+      eligible_sought_counseling == 1 | 
+      (eligible_anxiety_1 + eligible_anxiety_2 >= 4) | 
+      (eligible_depress_1 + eligible_depress_2 >= 4) | 
+      (eligible_sch_connect_1 < 3 | eligible_sch_connect_2 < 3) | 
+      eligible_self_harm > 0 | 
+      eligible_sleep_problem_1 > 1,
+      1, 0)
+  )
+
+# Manual fixes due to redcap entry error
+# Check this box file for a complete list and description
+# https://nyu.box.com/s/8vj7nrljzxsg7s8z68ilm80vgm2bey2m
+
+dat <- dat |> 
+  mutate(p_informed_consent_form_parent_complete = 
+           if_else(p_wecare_id == "K-F0005-C-S", 
+                   0, p_informed_consent_form_parent_complete)) |> 
+  mutate(age_group = 
+           if_else(wecare_id == "K-F0006-Y-S",
+                   "18+", age_group)) |> 
+  mutate(contact_form_youth_complete = 
+           if_else(wecare_id == "K-F0006-Y-S",
+                   2, contact_form_youth_complete)) |> 
+  mutate(ps_signature = 
+           if_else(wecare_id == "K-F0006-Y-S",
+                   "signature_unavailable", ps_signature))
 
 
--- R/table_functions.R
+++ R/table_functions.R
@@ -14,8 +14,12 @@ t_enrollment <- dat |>
   summarize(
     n_completed_contact_form = sum((age_group == "12-17" & p_contact_form_parent_complete == 2) | 
                                      (age_group == "18+" & contact_form_youth_complete == 2), na.rm = TRUE),
-    n_passed_eligibility_screen = sum(eligibility_screen_complete == 2, na.rm = TRUE),
-    n_passed_eligibility_survey = sum(eligibility_survey_complete == 2, na.rm = TRUE),
+    n_completed_eligibility_screen = sum(eligibility_screen_complete == 2, na.rm = TRUE),
+    n_completed_eligibility_survey = sum(eligibility_survey_complete == 2, na.rm = TRUE),
+    # the first field in eligiblity survey is required, so if eligible_visited_ed is not NA, 
+    # it means the person passed the eligiblity screen 
+    n_passed_eligibility_screen = sum(is.na(eligible_visited_ed) == F, na.rm = TRUE),
+    n_passed_eligibility_survey = sum(meet_minimal_risk == 1, na.rm = TRUE),
     n_consented = sum((age_group == "12-17" & p_informed_consent_form_parent_complete == 2) | 
                         (age_group == "18+" & informed_consent_form_youth_complete == 2), na.rm = TRUE),
     .groups = 'drop'  # Drop the grouping once summarization is done
@@ -23,7 +27,9 @@ t_enrollment <- dat |>
   # Ensure all combinations of site_id and age_group appear in the results
   complete(site_id = c("Harlem", "Kings County"), age_group = c("12-17", "18+"), fill = list(
     n_completed_contact_form = 0, 
+    n_completed_eligibility_screen = 0,
     n_passed_eligibility_screen = 0,
+    n_completed_eligibility_survey = 0,
     n_passed_eligibility_survey = 0,
     n_consented = 0
   )) |> 
@@ -37,11 +43,13 @@ t_enrollment <- dat |>
 # Rename
 names(t_enrollment) <- c("", "Age Group",
                "Number of youth who completed the contact form",
+               "Number of youth who completed the eligibility screen",
                "Number of youth who passed the eligibility screen",
+               "Number of youth who completed the eligibility survey",
                "Number of youth who passed the eligibility survey",
                "Number of youth who consented",
-               "Percent of youth eligible among those who completed the contact form",
-               "Percent of youth eligible among those who passed the eligibility survey")
+               "Percent of youth who consented among those who completed the contact form",
+               "Percent of youth who consented among those who passed the eligibility survey")
 
 # Transpose
 t_enrollment <- t_enrollment |> 
@@ -119,15 +127,132 @@ ft_weekly_enrollment <- flextable(t_weekly_enrollment) |>
   bold(i = 1:2, bold = TRUE, part = "body") |> 
   merge_h(i = 1)
 
-# -------- Table 3: Treatment/Control Group Assignment --------
+# -------- Table 3: Detailed Recruitment Information --------
+
+t_recruitment <- dat |>
+  # Ensure grouping is done by both site_id and the new age_group column
+  group_by(site_id) |>
+  # Use summarise() to compute summaries specific to each group
+  summarize(
+    n_completed_contact_form = sum((age_group == "12-17" & p_contact_form_parent_complete == 2) | 
+                                     (age_group == "18+" & contact_form_youth_complete == 2), na.rm = TRUE),
+    # the first field in eligiblity survey is required, so if eligible_visited_ed is not NA, 
+    # it means the person passed the eligiblity screen 
+    n_passed_eligibility_screen = sum(is.na(eligible_visited_ed) == F, na.rm = TRUE),
+    n_passed_eligibility_survey = sum(meet_minimal_risk == 1, na.rm = TRUE),
+    n_consented = sum((age_group == "12-17" & p_informed_consent_form_parent_complete == 2) | 
+                        (age_group == "18+" & informed_consent_form_youth_complete == 2), na.rm = TRUE),
+
+    # n_not_completed_contact_form = sum(!((age_group == "12-17" & p_contact_form_parent_complete == 2) | 
+    #                                        (age_group == "18+" & contact_form_youth_complete == 2)), na.rm = TRUE),
+    # n_not_passed_eligibility_screen = sum(!(is.na(eligible_visited_ed)) == F, na.rm = TRUE),
+    # n_not_passed_eligibility_survey = sum(!(meet_minimal_risk == 1), na.rm = TRUE),
+    # n_not_consented = sum((age_group == "12-17" & p_informed_consent_form_parent_complete != 2) & 
+    #                           (age_group == "18+" & informed_consent_form_youth_complete != 2), na.rm = TRUE),
+    
+    n_not_hear_more = sum(!((age_group == "12-17" & p_ps_hear_more == 1) | 
+                              (age_group == "18+" & ps_hear_more == 1)), na.rm = TRUE),
+    n_not_willing_to_contact = sum(!((age_group == "12-17" & p_ps_willing_to_contact == 1) | 
+                                       (age_group == "18+" & ps_willing_to_contact == 1)), na.rm = TRUE),
+    n_not_p_ps_signature = sum(age_group == "12-17" & is.na(p_ps_signature), na.rm = T),
+    n_not_ps_signature = sum(age_group == "18+" & is.na(ps_signature), na.rm = T),
+    
+    n_not_screen_age = sum(!(screen_age >= 12 & screen_age <= 19), na.rm = T),
+    n_not_screen_caregiver_present = sum(screen_age >= 12 & screen_age <= 17 & screen_caregiver_present == 0, na.rm = T),
+    n_not_screen_race_eligible = sum(!(screen_race_eligible == 1), na.rm = T),
+    n_not_screen_has_cell_phone = sum(!(screen_has_cell_phone == 1), na.rm = T),
+    n_not_screen_speak_read_english = sum(!(screen_speak_read_english == 1), na.rm = T),
+    n_seek_therapy = sum(screen_in_treatment == 1 & screen_seek_therapy == 1, na.rm = T),
+    n_screen_self_repeat = sum(screen_self_repeat == 1, na.rm = T),
+    n_screen_sibling_repeat = sum(screen_sibling_repeat == 2, na.rm = T),
+    n_not_meet_minimal_risk = sum(!meet_minimal_risk, na.rm = T),
+    n_total = n(),
+    .groups = 'drop'  # Drop the grouping once summarization is done
+  ) |> 
+  # Ensure all combinations of site_id and age_group appear in the results
+  complete(site_id = c("Harlem", "Kings County"), fill = list(
+    n_completed_contact_form = 0, 
+    n_passed_eligibility_screen = 0,
+    n_passed_eligibility_survey = 0,
+    n_consented = 0,
+    # n_not_completed_contact_form = 0, 
+    # n_not_passed_eligibility_screen = 0,
+    # n_not_passed_eligibility_survey = 0,
+    # n_not_consented = 0,
+    n_not_hear_more = 0,
+    n_not_willing_to_contact = 0,
+    n_not_p_ps_signature = 0,
+    n_not_ps_signature = 0,
+    n_not_screen_age = 0,
+    n_not_screen_caregiver_present = 0,
+    n_not_screen_race_eligible = 0,
+    n_not_screen_race_eligible = 0,
+    n_not_screen_has_cell_phone = 0,
+    n_not_screen_speak_read_english = 0,
+    n_seek_therapy = 0,
+    n_screen_self_repeat = 0,
+    n_screen_sibling_repeat = 0,
+    n_not_meet_minimal_risk = 0,
+    n_total = 0
+  )) 
+
+# Transpose
+t_recruitment <- t_recruitment |> 
+  t() |> 
+  as.data.frame() |> 
+  rownames_to_column()
+
+# Rename
+t_recruitment$rowname <- c(
+  "",
+  "Number of youth who completed the contact form",
+  "Number of youth who passed the eligibility screen",
+  "Number of youth who passed the eligibility survey",
+  "Number of youth who consented",
+  
+  # "Number of youth who DID NOT complete the contact form",
+  # "Number of youth who DID NOT pass the eligibility screen",
+  # "Number of youth who DID NOT pass the eligibility survey",
+  # "Number of youth who DID NOT consent",
+  
+  "Number of youth who were NOT willing to hear more about the study",
+  "Number of youth who were NOT willing to be contacted",
+  "Number of caregivers (with youth aged 12-17) who DID NOT consent in the contact form",
+  "Number of youth aged 18+ who DID NOT consent in the contact form",
+  
+  "Number of youth who were either below 12 or above 19",
+  "Number of youth aged 12-17 whose caregiver was NOT present",
+  "Number of youth whose race was NOT Black",
+  "Number of youth who DID NOT have a cellphone to receive texts",
+  "Number of youth who were NOT able to speak or read in English",
+  "Number of youth who has sought therapy in the past week",
+  "Number of youth who were currently enrolled in WeCare",
+  "Number of youth who had another youth family member in WeCare",
+  "Number of youth who DID NOT the minimal risk criteria",
+  "Total number of youth approached"
+)
+
+t_recruitment <- t_recruitment |> 
+  add_row(rowname = "General Recruitment Information", .after = 1) |> 
+  add_row(rowname = "Ineligible Based on the First Screen (Information from the Contact Form)", .after = 6) |> 
+  add_row(rowname = "Ineligible Based on the Second Screen (Information from Youth Eligibility Screen and Survey)", .after = 11)
+
+# Create flextable
+ft_recruitment <- flextable(t_recruitment) |> 
+  width(width = 1.5) |>
+  width(j = 1, width = 6) |>
+  delete_part(part = "header") |>
+  hline_top(j = 1:ncol(t_recruitment) , part = "body") |>
+  hline(i = c(1, 6, 11, 21) , part = "body") |> 
+  align(align = "center", part = "header") |> 
+  align(j = 2:3, align = "center", part = "body") |> 
+  bold(i = c(1, 2, 7, 12), bold = TRUE, part = "body") |> 
+  italic(i = 22, italic = TRUE, part = "body") |> 
+  merge_h(i = 1)
+
+# -------- Table 4: Treatment/Control Group Assignment --------
 
 t_assignment <- dat %>%
-  rowwise() |> 
-  mutate(completed_survey = if_else(
-    any(!is.na(c_across(demo_grade_b:joy_youth_baseline_assessment_complete))),
-    1, 0
-  )) |> 
-  ungroup() |> 
   # Ensure grouping is done by both site_id and the new age_group column
   group_by(site_id, age_group, treatment) |>
   # Use summarise() to compute summaries specific to each group
@@ -150,7 +275,7 @@ t_assignment <- dat %>%
     n_cassy_positive = NA,
     n_received_cfs = NA
   )) |> 
-  mutate(treatment = if_else(is.na(treatment), "Unassigned", treatment))
+  filter(is.na(treatment) == F)
 
 # Rename
 names(t_assignment) <- c("", "Treatment Condition","Age Group",
@@ -181,9 +306,9 @@ ft_assignment <- flextable(t_assignment) |>
   merge_h(i = 1:2)
 
 
-# -------- Table 4: Triggered Safety Plan During Baseline --------
+# -------- Table 5: Triggered Risk Management Protocol During Baseline --------
 
-t_trigger_cfs <- dat |>
+t_trigger_rmp <- dat |>
   rowwise() |>
   mutate(
     cssrs_positive = if_else(
@@ -207,31 +332,31 @@ t_trigger_cfs <- dat |>
   ))
   
 # Rename
-names(t_trigger_cfs) <- c("", 
+names(t_trigger_rmp) <- c("", 
                          "Number of youth who verbally disclosed suicide ideation",
                          "Number of youth who screened positive on CSSRS",
                          "Number of youth who screened positive on CASSY"
 )
 
 # Transpose
-t_trigger_cfs <- t_trigger_cfs |> 
+t_trigger_rmp <- t_trigger_rmp |> 
   t() |> 
   as.data.frame() |> 
   rownames_to_column()
 
 # Create flextable
-ft_trigger_cfs <- flextable(t_trigger_cfs) |> 
+ft_trigger_cfs <- flextable(t_trigger_rmp) |> 
   width(width = 1.5) |>
   width(j = 1, width = 4) |>
   delete_part(part = "header") |>
-  hline_top(j = 1:ncol(t_trigger_cfs) , part = "body") |>
+  hline_top(j = 1:ncol(t_trigger_rmp) , part = "body") |>
   hline(i = 1 , part = "body") |> 
   align(align = "center", part = "header") |> 
   align(align = "center", part = "body") |> 
   bold(i = 1, bold = TRUE, part = "body")
 
 
-# -------- Table 5: Demographics of Enrolled Subjects (DSMB Chart) --------
+# -------- Table 6: Demographics of Enrolled Subjects (DSMB Chart) --------
 t_demo <- dat |> 
   group_by(site_id, treatment) |>
   summarize(
@@ -381,144 +506,7 @@ ft_demo <- flextable(t_demo) |>
   italic(i = c(6, 10, 21, 32), italic = TRUE, part = "body") |> 
   merge_h(i = 1)
 
-  
-# -------- Table 6: Detailed Recruitment Information --------
-
-t_recruitment <- dat |>
-  rowwise() |>
-  mutate(screen_race_eligible = if_any(c(screen_race_1, screen_race_2, screen_race_3, screen_race_4), 
-                                       ~ .x == 1)) |>
-  ungroup() |> 
-  # Ensure grouping is done by both site_id and the new age_group column
-  group_by(site_id) |>
-  # Use summarise() to compute summaries specific to each group
-  summarize(
-    n_completed_contact_form = sum((age_group == "12-17" & p_contact_form_parent_complete == 2) | 
-                                     (age_group == "18+" & contact_form_youth_complete == 2), na.rm = TRUE),
-    n_passed_eligibility_screen = sum(eligibility_screen_complete == 2, na.rm = TRUE),
-    n_passed_eligibility_survey = sum(eligibility_survey_complete == 2, na.rm = TRUE),
-    n_consented = sum((age_group == "12-17" & p_informed_consent_form_parent_complete == 2) | 
-                        (age_group == "18+" & informed_consent_form_youth_complete == 2), na.rm = TRUE),
-    
-    n_not_completed_contact_form = sum(!((age_group == "12-17" & p_contact_form_parent_complete == 2) | 
-                                     (age_group == "18+" & contact_form_youth_complete == 2)), na.rm = TRUE),
-    n_not_passed_eligibility_screen = sum(!(eligibility_screen_complete == 2), na.rm = TRUE),
-    n_not_passed_eligibility_survey = sum(!(eligibility_survey_complete == 2), na.rm = TRUE),
-    n_not_consented = sum(!((age_group == "12-17" & p_informed_consent_form_parent_complete == 2) | 
-                        (age_group == "18+" & informed_consent_form_youth_complete == 2)), na.rm = TRUE),
-    
-    n_not_hear_more = sum(!((age_group == "12-17" & p_ps_hear_more == 1) | 
-                              (age_group == "18+" & ps_hear_more == 1)), na.rm = TRUE),
-    n_not_willing_to_contact = sum(!((age_group == "12-17" & p_ps_willing_to_contact == 1) | 
-                                       (age_group == "18+" & ps_willing_to_contact == 1)), na.rm = TRUE),
-    n_not_p_ps_signature = sum(age_group == "12-17" & is.na(p_ps_signature)),
-    n_not_ps_signature = sum(age_group == "18+" & is.na(ps_signature)),
-    n_not_screen_age = sum(!(screen_age >= 12 & screen_age <= 19)),
-    n_not_screen_caregiver_present = sum(screen_age >= 12 & screen_age <= 17 & screen_caregiver_present == 0),
-    n_not_screen_race_eligible = sum(!(screen_race_eligible == 1)),
-    n_not_screen_race_eligible = sum(!(screen_race_eligible == 1)),
-    n_not_screen_has_cell_phone = sum(!(screen_has_cell_phone == 1)),
-    n_not_screen_speak_read_english = sum(!(screen_speak_read_english == 1)),
-    n_seek_therapy = sum(screen_in_treatment == 1 & screen_seek_therapy == 1),
-    n_screen_self_repeat = sum(screen_self_repeat == 1),
-    n_screen_sibling_repeat = sum(screen_sibling_repeat == 2),
-    n_meet_minimal_risk = sum(!(
-        eligible_visited_ed == 1 | 
-        eligible_hospitalized == 1 | 
-        eligible_taken_med == 1 | 
-        eligible_received_therapy == 1 | 
-        eligible_sought_counseling == 1 | 
-        (eligible_anxiety_1 + eligible_anxiety_2 >= 4) | 
-        (eligible_depress_1 + eligible_depress_2 >= 4) | 
-        (eligible_sch_connect_1 < 3 | eligible_sch_connect_2 < 3) | 
-        eligible_self_harm > 0 | 
-        eligible_sleep_problem_1 > 1)),
-    n_total = n(),
-    .groups = 'drop'  # Drop the grouping once summarization is done
-  ) |> 
-  # Ensure all combinations of site_id and age_group appear in the results
-  complete(site_id = c("Harlem", "Kings County"), fill = list(
-    n_completed_contact_form = 0, 
-    n_passed_eligibility_screen = 0,
-    n_passed_eligibility_survey = 0,
-    n_consented = 0,
-    n_not_completed_contact_form = 0, 
-    n_not_passed_eligibility_screen = 0,
-    n_not_passed_eligibility_survey = 0,
-    n_not_consented = 0,
-    n_not_hear_more = 0,
-    n_not_willing_to_contact = 0,
-    n_not_p_ps_signature = 0,
-    n_not_ps_signature = 0,
-    n_not_screen_age = 0,
-    n_not_screen_caregiver_present = 0,
-    n_not_screen_race_eligible = 0,
-    n_not_screen_race_eligible = 0,
-    n_not_screen_has_cell_phone = 0,
-    n_not_screen_speak_read_english = 0,
-    n_seek_therapy = 0,
-    n_screen_self_repeat = 0,
-    n_screen_sibling_repeat = 0,
-    n_meet_minimal_risk = 0,
-    n_total = 0
-  )) 
-
-# Transpose
-t_recruitment <- t_recruitment |> 
-  t() |> 
-  as.data.frame() |> 
-  rownames_to_column()
-
-# Rename
-t_recruitment$rowname <- c(
-  "",
-  "Number of youth who completed the contact form",
-  "Number of youth who passed the eligibility screen",
-  "Number of youth who passed the eligibility survey",
-  "Number of families (youth and/or caregivers) who consented",
-  
-  "Number of youth who DID NOT complete the contact form",
-  "Number of youth who DID NOT pass the eligibility screen",
-  "Number of youth who DID NOT pass the eligibility survey",
-  "Number of youth who DID NOT consent",
-  
-  "Number of youth who were NOT willing to hear more about the study",
-  "Number of youth who were NOT willing to be contacted",
-  "Number of parents (with youth aged 12-17) who DID NOT consent in the contact form",
-  "Number of youth aged 18-19 who DID NOT consent in the contact form",
-  
-  "Number of youth who were either below 12 or above 19",
-  "Number of youth aged 12-17 whose caregiver was NOT present",
-  "Number of youth whose race was NOT Black",
-  "Number of youth who DID NOT have a cellphone to receive texts",
-  "Number of youth who were NOT able to speak or read in English",
-  "Number of youth who has sought therapy in the past week",
-  "Number of youth who were currently enrolled in WeCare",
-  "Number of youth who had another youth family member in WeCare",
-  "Number of youth who DID NOT the minimal risk criteria",
-  "Total"
-)
-
-t_recruitment <- t_recruitment |> 
-  add_row(rowname = "General Recruitment Information", .after = 1) |> 
-  add_row(rowname = "Ineligible Based on the First Screen (Information from the Contact Form)", .after = 10) |> 
-  add_row(rowname = "Ineligible Based on the Second Screen (Information from Youth Eligibility Screen and Survey)", .after = 15)
-
-# Create flextable
-ft_recruitment <- flextable(t_recruitment) |> 
-  width(width = 1.5) |>
-  width(j = 1, width = 6) |>
-  delete_part(part = "header") |>
-  hline_top(j = 1:ncol(t_recruitment) , part = "body") |>
-  hline(i = c(1, 10, 15, 25) , part = "body") |> 
-  align(align = "center", part = "header") |> 
-  align(j = 2:3, align = "center", part = "body") |> 
-  bold(i = c(1, 2, 11, 16), bold = TRUE, part = "body") |> 
-  italic(i = 26, italic = TRUE, part = "body") |> 
-  merge_h(i = 1)
-
-
-# -------- Table 7: Enrollment Table: Projected vs. Actual by Time (cumulative over time) --------
+  # -------- Table 7: Enrollment Table: Projected vs. Actual by Time (cumulative over time) --------
 
 t_monthly_enrollment_actual <- dat |>
   mutate(date_of_enrollment_all = coalesce(p_date_of_enrollment, date_of_enrollment, screen_doe)) |>
@@ -529,17 +517,16 @@ t_monthly_enrollment_actual <- t_monthly_enrollment_actual |>
   group_by(site_id, month_year_of_enrollment) |>
   # Use summarise() to compute summaries specific to each group
   summarize(
-    n_consented = sum((age_group == "12-17" & p_informed_consent_form_parent_complete == 2) | 
-                        (age_group == "18+" & informed_consent_form_youth_complete == 2), na.rm = TRUE),
+    n_randomized = sum(is.na(treatment) == F, na.rm = TRUE),
     .groups = 'drop'  # Drop the grouping once summarization is done
   ) |> 
   # Ensure all combinations of site_id and age_group appear in the results
   complete(site_id = c("Harlem", "Kings County"), 
            month_year_of_enrollment = na.omit(unique(t_monthly_enrollment_actual$month_year_of_enrollment)),
            fill = list(
-             n_consented = 0
+             n_randomized = 0
            )) |> 
-  pivot_wider(names_from = site_id, values_from = n_consented) |> 
+  pivot_wider(names_from = site_id, values_from = n_randomized) |> 
   mutate(actual_enrollment = sum(c_across(c(Harlem, `Kings County`)), na.rm = T),
          .after = month_year_of_enrollment)
 
